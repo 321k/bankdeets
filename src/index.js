8f4197ae9da2474081d52c613fe3f55993d467e9
Erik Edin <erik.johannes.edin@gmail.com>
2019-10-23 15:54:56 +0100

    fixing country selector

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 import BankDeets from './components/BankDeets.js'
 export default BankDeets
+
+ReactDOM.render(<BankDeets />, document.getElementById('root'));
