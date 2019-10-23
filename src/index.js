7036b6b01737a161fb25a524adab6322b6c3d1a6
Erik Edin <erik.johannes.edin@gmail.com>
2019-10-23 15:58:28 +0100

    removing some bits

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 import React from "react";
-import ReactDOM from 'react-dom';
 import BankDeets from './components/BankDeets.js'
 export default BankDeets
-
-ReactDOM.render(<BankDeets />, document.getElementById('root'));
